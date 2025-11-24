55be3ac546fddc1ce3246e3870518edfcd306164
NaufalSF-Max <naufalsafiqq@gmail.com>
2025-11-24 13:44:10 +0700

    feat: install laravel breeze & migration database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('npm')->unique();
+            $table->string('kelas');
+            $table->string('mata_kuliah')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
